ccba019f6b798145babfc2a3f618ba1a4558ef7e
sehee <seeeh0128@gmail.com>
2022-06-29 11:05:40 +0900

    header와 nav 골격

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import { Header } from './Header';
+import { Link } from 'react-router-dom'
 
-function App() {
+function Nav(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <nav>
+      <ol>
+        <li><Link to='/read/1'>html</Link></li>
+      </ol>
+    </nav>
+  )
+}
+
+function App(){
+  return (
+    <div>
+      <Header></Header>
+      <Nav></Nav>
     </div>
   );
 }
