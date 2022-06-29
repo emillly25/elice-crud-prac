764987ee982010baf68112474b18a89ee44ce3c2
sehee <seeeh0128@gmail.com>
2022-06-29 13:27:44 +0900

    Read 컴포넌트에서 해당 id에 맞는 내용 가져오기

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import { Header } from './Header';
-import { Link, Routes, Route } from 'react-router-dom'
+import { Link, Routes, Route, useParams } from 'react-router-dom'
 import { useEffect, useState } from 'react'
 import { Welcome } from './Welcome';
 
@@ -16,10 +16,23 @@ function Nav(props){
 }
 
 function Read(){
+  const params = useParams()
+  const id = Number(params.id)
+  const [topic, setTopic] = useState({title: null, body: null})
+  async function refresh(){
+    const res = await fetch('/topics/' + id)
+    const data = await res.json()
+    setTopic(data)
+  } 
+
+  useEffect(()=>{
+    refresh()
+  },[id])
+
   return(
     <article>
-      <h2>Read</h2>
-      Hello, Read!
+      <h2>{topic.title}</h2>
+      {topic.body}
     </article>
   )
 }
@@ -35,16 +48,15 @@ function App(){
 
   useEffect(()=>{
     refresh()
-  
   },[])
-
+  
   return (
     <div>
       <Header></Header>
       <Nav data={topics}></Nav>
       <Routes>
         <Route path='/' element={<Welcome/>}/>
-        <Route path='/read/1' element={<Read/>}/>
+        <Route path='/read/:id' element={<Read/>}/>
       </Routes>
     </div>
   );
